bd3c9fecb2ea5bed8737c6737315d4b050285afb
Josue Carrera <josuecarrera13@hotmail.com>
2026-03-31 12:07:52 -0500

    fix: lang in vercel

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { getDictionary } from "@/src/lib/dictionaries";
 import Navbar from "@/src/components/layout/Navbar";
 
+// 1. DEFINIMOS EL TIPO AQUÍ (Esto soluciona el error "Cannot find name 'Locale'")
+type Locale = "es" | "en";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,16 +22,17 @@ export const metadata: Metadata = {
   description: "Portafolio profesional de Ingeniería de Software, DevOps y Ciberseguridad",
 };
 
+// 2. Mantenemos string aquí para que Next.js no dé error en el build de Vercel
 type Props = {
   children: React.ReactNode;
-  params: Promise<{ lang: 'es' | 'en' }>;
+  params: Promise<{ lang: string }>;
 }
 
 export default async function RootLayout({ children, params }: Props) {
-
   const { lang } = await params;
 
-  const dict = await getDictionary(lang)
+  // 3. Usamos "as Locale" para decirle a TS que confíe en que el string es válido
+  const dict = await getDictionary(lang as Locale);
 
   return (
     <html lang={lang} className="scroll-smooth">
@@ -37,7 +41,6 @@ export default async function RootLayout({ children, params }: Props) {
           text-slate-50`}
       >
         <Navbar dict={dict.navigation} />
-        
         {children}
       </body>
     </html>
